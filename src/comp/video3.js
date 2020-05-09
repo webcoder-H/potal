f20f1976806482011ae7a719b2109a67389b0ad7
Gaurav Tewari <54894783+tewarig@users.noreply.github.com>
2020-05-09 12:15:55 +0530

    minor fix

--- src/comp/video3.js
+++ src/comp/video3.js
@@ -786,7 +786,7 @@ Communication_Systems: [
         <>
         <h3>{title}</h3>
         <div class="video-container">
-  <iframe width="40%" height="25%" src={"//www.youtube.com/embed/"+ vid +"?rel=0"} allowFullScreen="allowFullScreen" and frameBorder="0" ></iframe>
+  <iframe width="40%" height="25%" src={"//www.youtube.com/embed/"+ vid +"?rel=0"} allowFullScreen="allowFullScreen" frameBorder="0" ></iframe>
 </div>
 </>
 )
