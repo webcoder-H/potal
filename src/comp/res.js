d9ceeb87b51b3a88cd11a5d0d4f3c4c4e667b17d
KushagraMathur20 <56122937+KushagraMathur20@users.noreply.github.com>
2020-05-11 10:46:16 +0530

    Update res.js

--- src/comp/res.js
+++ src/comp/res.js
@@ -25,7 +25,6 @@ class Res extends React.Component {
             <div class="card">
        <div class="card-image">
          <img src={Uni}/>
-         <span class="card-title">Resources For University Students</span>
        </div>
        <div class="card-content">
          <p>This section contains all the free resouces from the youtube as well as other resouces contributed by University Students and Teachers.</p>
@@ -42,7 +41,6 @@ class Res extends React.Component {
         <Link to="/school">
    <div class="card-image">
      <img src={Sch}/>
-     <span class="card-title">Resources For School Students</span>
    </div>
    <div class="card-content">
    <p>This section contains all the free resouces from the youtube as well as other resouces contributed by other School Students and Teachers.</p>
@@ -64,7 +62,6 @@ class Res extends React.Component {
             <div class="card">
        <div class="card-image">
          <img src={cbse1}/>
-         <span class="card-title">This one is For 12th Class CBSE students</span>
        </div>
        <div class="card-content">
          <p>This section contains all the free resouces from the youtube as well as other resouces contributed by University Students and Teachers.</p>
@@ -81,7 +78,6 @@ class Res extends React.Component {
         <Link to="/cbse10">
    <div class="card-image">
      <img src={cbse2}/>
-     <span class="card-title">This one is for 10th class Students </span>
    </div>
    <div class="card-content">
    <p>This section contains all the free resouces from the youtube as well as other resouces contributed by other School Students and Teachers.</p>
